a5c422183e99bffefb26121d6646d201c1b394ac
fikri <fikri.dulumina@untad.ac.id>
2022-09-30 03:32:27 +0800

    FEAT: get data Dosen

--- index.js
+++ index.js
@@ -1,17 +1,18 @@
-const express = require('express');
-const bodyParser = require('body-parser');
-const app = express();
-const mysql = require('mysql');
-const md5 = require('md5');
-const jwt = require('jsonwebtoken');
+const { select } = require("@nodejscart/mysql-query-builder");
+const express = require("express");
+const bodyParser = require("body-parser");
+const mysql = require("mysql");
+const md5 = require("md5");
+const jwt = require("jsonwebtoken");
 const now = Date.now();
 // import * as dotenv from 'dotenv';
 // dotenv.config()
 
-require('dotenv').config()
+require("dotenv").config();
 // console.log(process.env)
 
-const PORT = process.env.APP_PORT
+const app = express();
+const PORT = process.env.APP_PORT;
 // parse application/json
 app.use(bodyParser.json());
 
@@ -19,73 +20,78 @@ const config = {
   host: process.env.DB_HOST,
   user: process.env.DB_USER,
   password: process.env.DB_PASS,
-  database: process.env.DB_NAME
+  database: process.env.DB_NAME,
 };
 //create database connection
 const conn = mysql.createConnection(config);
 // console.log(config);
 
 //connect to database
-conn.connect((err) =>{
+conn.connect((err) => {
   // if(err) throw err;
   if (err) {
-    return console.error('error: ' + err.message);
+    return console.error("error: " + err.message);
   }
-  console.log('Mysql Connected...');
+  console.log("Mysql Connected...");
 });
 
 // router
-app.post('/auth/login', (req, res) => {
-  const username = req.body.username;
-  const password = md5(req.body.password)
-  let sql = "SELECT * FROM auth WHERE username='"+username+"' AND password='"+password+"'";
-  let query = conn.query(sql, (err, results) => {
+app.get("/user/mahasiswa/:userId", async (req, res) => {
+  const userId = req.params.userId;
+  let sql =
+    "SELECT * FROM users u inner join mahasiswa m on m.user_id=u.user_id WHERE u.user_id='" +
+    userId +
+    "'";
+  conn.query(sql, (err, results) => {
     if (err) throw err;
-    if (results.length == 1) {
-      // console.log(results[0]);
-      const token = createJwt(results[0].user_id);
-      const data = {
-        accessToken: token
-      };
-      console.log(verify(token));
-      res.setHeader('content-type', 'application/json');
-      res.send(JSON.stringify({"status": 200, "error": null, "response": data}));
+    if (results.length <= 0) {
+      data = { status: 200, error: "Tidak ada data ditemukan", response: [] };
+      returnApi(res, data);
     } else {
-      res.setHeader('content-type', 'application/json');
-      res.send(JSON.stringify({"status": 401, "error": 'Unauthorized', "response": [] }));
+      data = { status: 200, error: null, response: results[0] };
+      returnApi(res, data);
     }
   });
 });
 
-app.post('/auth/verify', (req, res) => {
-  const token = req.body.token;
-  const tokenVerify = verify(token)
-  console.log(tokenVerify.status);
-  if (tokenVerify.status) {
-    res.setHeader('content-type', 'application/json');
-    res.send(JSON.stringify({"status": 200, "error": null, "response": tokenVerify.data}));
-  } else {
-    res.setHeader('content-type', 'application/json');
-    res.send(JSON.stringify({"status": 403, "error": "ExpiredToken", "response": tokenVerify.data}));
-  }
-
-})
+app.get("/user/dosen/:userId", async (req, res) => {
+  const userId = req.params.userId;
+  let sql =
+    "SELECT * FROM users u inner join dosen m on m.user_id=u.user_id WHERE u.user_id='" +
+    userId +
+    "'";
+  conn.query(sql, (err, results) => {
+    if (err) throw err;
+    if (results.length <= 0) {
+      data = { status: 200, error: "Tidak ada data ditemukan", response: [] };
+      returnApi(res, data);
+    } else {
+      data = { status: 200, error: null, response: results[0] };
+      returnApi(res, data);
+    }
+  });
+});
 
 // functions
+function returnApi(res, data) {
+  res.setHeader("content-type", "application/json");
+  res.send(JSON.stringify(data));
+}
+
 function verify(token) {
-  const payload = jwt.verify(token, process.env.JWT_SECRET_KEY)
-  if ((payload.exp - now) >0 ) {
+  const payload = jwt.verify(token, process.env.JWT_SECRET_KEY);
+  if (payload.exp - now > 0) {
     return {
-      status:true,
+      status: true,
       msg: "Tuken sudah sesuai",
       data: payload,
     };
   } else {
     return {
-      status:false,
+      status: false,
       msg: "token sudah tidak berlaku.",
-      data:[]
-    }
+      data: [],
+    };
   }
 }
 
@@ -93,15 +99,15 @@ function createJwt(userId) {
   let jwtSecretKey = process.env.JWT_SECRET_KEY;
   let data = {
     iat: now,
-    exp:now+(1000*60*60*24), // satu hari (24 jam)
+    exp: now + 1000 * 60 * 60 * 24, // satu hari (24 jam)
     // exp:now, // satu hari (24 jam)
     userId,
-  }
+  };
   const token = jwt.sign(data, jwtSecretKey);
   return token;
 }
 
 //Server listening
-app.listen(PORT,() =>{
-  console.log('Server started on port '+PORT+'...');
+app.listen(PORT, () => {
+  console.log("Server started on port " + PORT + "...");
 });
--- restful_db.sql
+++ restful_db.sql
@@ -1,55 +1,165 @@
 -- phpMyAdmin SQL Dump
--- version 4.1.14
--- http://www.phpmyadmin.net
+-- version 5.1.3
+-- https://www.phpmyadmin.net/
 --
--- Host: 127.0.0.1
--- Generation Time: 20 Jan 2019 pada 09.02
--- Versi Server: 5.6.17
--- PHP Version: 5.5.12
+-- Host: dbUser
+-- Waktu pembuatan: 29 Sep 2022 pada 19.05
+-- Versi server: 5.7.38
+-- Versi PHP: 8.0.15
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
 SET time_zone = "+00:00";
 
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
 /*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8 */;
+/*!40101 SET NAMES utf8mb4 */;
 
 --
--- Database: `restful_db`
+-- Database: `user`
 --
 
 -- --------------------------------------------------------
 
 --
--- Struktur dari tabel `product`
+-- Struktur dari tabel `dosen`
 --
 
-CREATE TABLE IF NOT EXISTS `auth` (
-  `auth_id` int(11) NOT NULL AUTO_INCREMENT,
-  `username` varchar(200) DEFAULT NULL,
-  `password` varchar(200) DEFAULT NULL,
-  `user_id` int(200) DEFAULT NULL,
-  PRIMARY KEY (`auth_id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=6 ;
+CREATE TABLE `dosen` (
+  `dosen_id` int(11) NOT NULL,
+  `user_id` int(11) NOT NULL,
+  `nip` bigint(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
--- Dumping data untuk tabel `product`
+-- Dumping data untuk tabel `dosen`
 --
 
-INSERT INTO `auth` (`auth_id`, `username`, `password`, `user_id`) VALUES
-(1, 'agus', '5787be38ee03a9ae5360f54d9026465f', 1),
-(2, 'memet', '5787be38ee03a9ae5360f54d9026465f', 2),
-(3, 'inal', '5787be38ee03a9ae5360f54d9026465f', 3),
-(4, 'bair', '5787be38ee03a9ae5360f54d9026465f', 4),
-(5, 'agri', '5787be38ee03a9ae5360f54d9026465f', 5),
-(6, 'noval', '5787be38ee03a9ae5360f54d9026465f', 6),
-(7, 'eja', '5787be38ee03a9ae5360f54d9026465f', 7),
-(8, 'mike', '5787be38ee03a9ae5360f54d9026465f', 8),
-(9, 'fikri', '5787be38ee03a9ae5360f54d9026465f', 9),
-(10, 'yusril', '5787be38ee03a9ae5360f54d9026465f', 10);
+INSERT INTO `dosen` (`dosen_id`, `user_id`, `nip`) VALUES
+(1, 6, 12457459679),
+(2, 7, 67367345662),
+(3, 8, 42353234756),
+(4, 9, 45342353454),
+(5, 10, 34534534553);
 
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `mahasiswa`
+--
+
+CREATE TABLE `mahasiswa` (
+  `mahasiswa_id` int(11) NOT NULL,
+  `user_id` int(11) NOT NULL,
+  `nim` int(11) NOT NULL,
+  `generation` int(11) NOT NULL,
+  `status` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data untuk tabel `mahasiswa`
+--
+
+INSERT INTO `mahasiswa` (`mahasiswa_id`, `user_id`, `nim`, `generation`, `status`) VALUES
+(1, 1, 12345, 2020, 1),
+(2, 3, 42341, 2020, 1),
+(3, 2, 67562, 2020, 1),
+(4, 4, 53453, 2020, 1),
+(5, 5, 34535, 2020, 1);
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `users`
+--
+
+CREATE TABLE `users` (
+  `user_id` int(11) NOT NULL,
+  `user_name` varchar(100) NOT NULL,
+  `user_adrdess` varchar(200) NOT NULL,
+  `user_type` int(11) NOT NULL COMMENT '1=Mahasiswa | 2=Dosen'
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data untuk tabel `users`
+--
+
+INSERT INTO `users` (`user_id`, `user_name`, `user_adrdess`, `user_type`) VALUES
+(1, 'Sadye', '160 Schurz Place', 1),
+(2, 'Joly', '0508 Clemons Drive', 1),
+(3, 'Fedora', '37742 Golden Leaf Avenue', 1),
+(4, 'Ebenezer', '91898 Nova Lane', 1),
+(5, 'Myrah', '83780 Vermont Point', 1),
+(6, 'Maudie', '16 Saint Paul Trail', 2),
+(7, 'Hervey', '65510 East Court', 2),
+(8, 'Marley', '611 Knutson Junction', 2),
+(9, 'Munmro', '13615 Milwaukee Hill', 2),
+(10, 'Cherish', '9925 Portage Plaza', 2);
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indeks untuk tabel `dosen`
+--
+ALTER TABLE `dosen`
+  ADD PRIMARY KEY (`dosen_id`),
+  ADD KEY `user_id` (`user_id`);
+
+--
+-- Indeks untuk tabel `mahasiswa`
+--
+ALTER TABLE `mahasiswa`
+  ADD PRIMARY KEY (`mahasiswa_id`),
+  ADD KEY `user_id` (`user_id`);
+
+--
+-- Indeks untuk tabel `users`
+--
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`user_id`);
+
+--
+-- AUTO_INCREMENT untuk tabel yang dibuang
+--
+
+--
+-- AUTO_INCREMENT untuk tabel `dosen`
+--
+ALTER TABLE `dosen`
+  MODIFY `dosen_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+
+--
+-- AUTO_INCREMENT untuk tabel `mahasiswa`
+--
+ALTER TABLE `mahasiswa`
+  MODIFY `mahasiswa_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+
+--
+-- AUTO_INCREMENT untuk tabel `users`
+--
+ALTER TABLE `users`
+  MODIFY `user_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
+
+--
+-- Ketidakleluasaan untuk tabel pelimpahan (Dumped Tables)
+--
+
+--
+-- Ketidakleluasaan untuk tabel `dosen`
+--
+ALTER TABLE `dosen`
+  ADD CONSTRAINT `dosen_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `users` (`user_id`);
+
+--
+-- Ketidakleluasaan untuk tabel `mahasiswa`
+--
+ALTER TABLE `mahasiswa`
+  ADD CONSTRAINT `mahasiswa_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `users` (`user_id`);
+COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
